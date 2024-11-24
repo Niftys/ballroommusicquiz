e43b945fad6d3d316949d2ec2d0d6524c1b134a4
Niftys <lowery.seth8@gmail.com>
2024-11-23 22:35:37 -0600

    add metadata

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+      reactStrictMode: true,
+  experimental: {
+    appDir: true,
+  },
+  env: {
+    // Add any other environment variables here
+  },
+  head: {
+    title: "Ballroom Music Quiz",
+    meta: [
+      { name: "description", content: "Test your ballroom music knowledge!" },
+    ],
+  },
+};
 
 export default nextConfig;
