b77cd0a7a2a754406b71117ed80c6dcb5a1e800d
Niftys <lowery.seth8@gmail.com>
2024-11-23 22:33:46 -0600

    add metadata

--- next.config.mjs
+++ next.config.mjs
@@ -2,17 +2,3 @@
 const nextConfig = {};
 
 export default nextConfig;
-
-module.exports = {
-    reactStrictMode: true,
-    experimental: {
-      appDir: true,
-    },
-    head: {
-      title: "Ballroom Music Quiz",
-      meta: [
-        { name: "description", content: "Do you know your partner dance genres?" },
-      ],
-    },
-  };
-  
