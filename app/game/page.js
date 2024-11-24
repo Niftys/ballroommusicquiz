6dd5df056b9616f83ec1b06092896206182901cb
Niftys <lowery.seth8@gmail.com>
2024-11-23 21:28:56 -0600

    gamecontent

--- app/game/page.js
+++ app/game/page.js
@@ -1,5 +1,5 @@
 'use client';
-import React, { useState, useEffect, useRef } from 'react';
+import React, { Suspense, useState, useEffect, useRef } from 'react';
 import { useSearchParams } from 'next/navigation';
 
 const COLORS = {
