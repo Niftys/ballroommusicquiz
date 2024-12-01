e6a99b3c64a81436af7d73d858167014463800a9
Niftys <lowery.seth8@gmail.com>
2024-12-01 00:09:04 -0600

    transitions, style changes

--- app/page.js
+++ app/page.js
@@ -16,27 +16,27 @@ export default function Home() {
           animate={{ opacity: 1, scale: 1 }}
           exit={{ opacity: 0, scale: 1.2 }}
           transition={{ ease: "easeOut", duration: 0.4 }}
-          className="grid md:grid-cols-2 grid-cols-1 gap-[100px] w-full max-w-[1200px] p5 box-border"
+          className="grid grid-cols-1 gap-5 w-full max-w-[1200px] p-5 md:grid-cols-2 md:gap-[100px]"
         >
           {/* Header */}
-          <header className="col-span-2 text-center">
-            <h1 className="font-bold font-megrim text-[5rem] text-[#ffc107] drop-shadow-lg">
+          <header className="text-center md:col-span-2">
+            <h1 className="font-bold font-megrim text-[3rem] text-[#ffc107] drop-shadow-lg md:text-[5rem]">
               Ballroom Music Quiz
             </h1>
           </header>
 
           {/* Instructions */}
           <section className="flex flex-col justify-center items-center text-[#e0e0e0] border border-[#ffc107] rounded-lg bg-black/10 p-5 shadow-2xl gap-5">
-            <p className="font-bold text-[1.5rem] text-center mb-2">
+            <p className="font-bold text-[1.2rem] text-center mb-2 md:text-[1.5rem]">
               Don&apos;t know how to play?
             </p>
-            <ul className="space-y-3 text-center">
+            <ul className="space-y-3 text-center text-sm md:text-base">
               <li>♫ You&apos;ll hear a clip of a song from any ballroom style ♫</li>
               <li>💭 Guess the correct dance style associated with the song 💭</li>
               <li>✰ Score points for every correct answer within the time limit ✰</li>
               <li>✖ Lose lives when you don&apos;t guess the style in time ✖</li>
             </ul>
-            <div className="flex justify-center mt-3 my-3">
+            <div className="flex justify-center mt-3">
               <Link href="/leaderboard">
                 <button className="px-6 py-3 rounded-lg bg-[#333] text-[#f5f5f5] hover:bg-[#222] transition-all shadow-2xl">
                   View Leaderboard
@@ -47,7 +47,7 @@ export default function Home() {
 
           {/* Settings */}
           <section className="flex flex-col justify-center items-center text-[#e0e0e0] border border-[#ffc107] rounded-lg bg-black/10 p-5 shadow-lg gap-4">
-            <p className="font-bold text-[1.5rem] text-center mb-4">
+            <p className="font-bold text-[1.2rem] text-center mb-4 md:text-[1.5rem]">
               Choose Settings:
             </p>
             <div className="flex justify-center gap-3">
